9227bd81bff22ede3073f55b9b8c06511831a6d1
aravindas4 <ash.g.education@gmail.com>
2023-02-18 12:31:56 +0530

    Add trapping water solution

--- leets/trapping-rain-water.py
+++ leets/trapping-rain-water.py
@@ -1,5 +1,8 @@
 from typing import List
 
+# https://www.youtube.com/watch?v=ZI2z5pq0TqA&t=882s
+# https://leetcode.com/problems/trapping-rain-water/
+
 class Solution:
     def trap(self, height: List[int]) -> int:
         length = len(height)
@@ -18,13 +21,17 @@ class Solution:
         while left < right:
             if max_left < max_right:
                 left += 1
+                delta = max_left - height[left]
+                if delta > 0:
+                    water += delta
                 max_left = max(max_left, height[left])
-                water += max_left - height[left]
             else:
                 right -= 1
+                delta = max_right - height[right]
+                if delta:
+                    water += delta
                 max_right = max(max_right, height[right])
-                water += max_right - height[right]
         return water
 
 s = Solution()
-print(s.trap([5,4,1,2]))
+print(s.trap([4,2,0,3,2,5]))
